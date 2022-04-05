bdf8b657a4120540692567cf050c65967bf83d3e
Hoon Wee <mrgravity817@gmail.com>
2022-04-05 17:24:39 +0900

    Config for static files

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,23 @@ const path = require("path");
 const HtmlPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
+const getHtmlPlugins = (chunks) => {
+  return chunks.map(
+    (chunk) =>
+      new HtmlPlugin({
+        title: "React Typescript Extension",
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
+};
+
 module.exports = {
   mode: "development",
   devtool: "cheap-module-source-map",
   entry: {
     popup: path.resolve("src/popup/index.tsx"),
+    options: path.resolve("src/options/index.tsx"),
   },
   module: {
     rules: [
@@ -15,22 +27,26 @@ module.exports = {
         test: /\.tsx?$/,
         exclude: /node_modules/,
       },
+      {
+        use: ["style-loader", "css-loader"],
+        test: /\.css$/i,
+      },
+      {
+        type: "asset/resource",
+        test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+      },
     ],
   },
   plugins: [
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve("src/manifest.json"),
+          from: path.resolve("src/static"),
           to: path.resolve("dist"),
         },
       ],
     }),
-    new HtmlPlugin({
-      title: "React Extension",
-      filename: "popup.html",
-      chunks: ["popup"],
-    }),
+    ...getHtmlPlugins(["popup", "options"]),
   ],
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
@@ -38,5 +54,10 @@ module.exports = {
   output: {
     filename: "[name].js",
     path: path.resolve("dist"),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
   },
 };
