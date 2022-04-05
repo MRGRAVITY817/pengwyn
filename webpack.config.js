9dddd8699c40e539970b1e15bb132aae01959887
Hoon Wee <mrgravity817@gmail.com>
2022-04-05 17:07:52 +0900

    Add react and typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.tsx",
+  devtool: "cheap-module-source-map",
+  entry: {
+    popup: path.resolve("src/popup/index.tsx"),
+  },
   module: {
     rules: [
       {
@@ -12,11 +17,26 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve("src/manifest.json"),
+          to: path.resolve("dist"),
+        },
+      ],
+    }),
+    new HtmlPlugin({
+      title: "React Extension",
+      filename: "popup.html",
+      chunks: ["popup"],
+    }),
+  ],
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    path: path.resolve("dist"),
   },
 };
